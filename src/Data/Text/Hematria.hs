06b81d341b559e122ac2bae1c3673d1f10536835
David Sánchez <david.sanchez.lt@gmail.com>
2021-12-07 02:23:40 +0000

    (WIP) Random word sampling

--- src/Data/Text/Hematria.hs
+++ src/Data/Text/Hematria.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.Text.Hematria where
 
 import Cache (cacheAvailable, updateCache)
@@ -7,25 +9,29 @@ import qualified Data.IntMap.Strict as IM
 import Data.Maybe (fromMaybe)
 import qualified Data.Set as S
 import qualified Data.Text as T
-import Data.Text.Hematria.Cipher (Cipher, getCharValue)
+import Data.Text.Hematria.Cipher (Cipher, computeNumericalValue, getCharValue, listCiphers, printNumericalValue)
 import Data.Text.Hematria.Dictionary
   ( Dictionary,
     DictionaryData,
     buildDictionary,
-    computeNumericalValue,
     getCipheredDictionary,
     getCipheredWords,
+    listDicts,
   )
+import GHC.Natural (Natural)
 import OptionsParser
-  ( Command (Update),
+  ( Command (List, Update, Value),
     Opts (Cmd, optCipher, optDictionary, optShow, word),
+    Resource (..),
     execParser,
     optsParser,
   )
+import System.Random (Random (randomR), RandomGen, getStdGen, uniformR)
+import Data.List (unfoldr)
 
 -- | Analyzes a word with the cipher and returns its numerical value
 --  and a list of words in the dictionary with the same value
-gematria :: Cipher -> DictionaryData -> T.Text -> (Int, Maybe [T.Text])
+gematria :: Cipher -> DictionaryData -> T.Text -> (Int, Maybe (S.Set T.Text))
 gematria c d w = (v, wordList)
   where
     v = computeNumericalValue c w
@@ -41,6 +47,9 @@ parseOptions = execParser optsParser
 
 applyCommand :: Opts -> IO ()
 applyCommand (Cmd Update) = updateCache
+applyCommand (Cmd (List Dictionary)) = listDicts
+applyCommand (Cmd (List Cipher)) = listCiphers
+applyCommand (Cmd (Value c w)) = getConfig >>= \conf -> printNumericalValue (fromMaybe (cipher conf) c) w
 applyCommand opts = cacheAvailable >>= bool (error "No cache found") (performGematria opts)
 
 performGematria :: Opts -> IO ()
@@ -53,7 +62,19 @@ performGematria opts = do
   dd <- getCipheredDictionary c d
   let (numValue, wordList) = gematria c dd w
   -- Perform random choosing, whith 0 selecting all words
-  putStrLn $ "The numerical value of " <> T.unpack w <> " is " <> show numValue
+  putStrLn $ "The numerical value of " <> T.unpack w <> " is " <> show numValue <> "."
   case wordList of
     Nothing -> putStrLn "No words in the dictionary have the same numerical value"
-    Just ws -> putStrLn $ "The words in the dictionary with the same numerical value are: " <> show ws
+    Just ws -> printWords n ws -- putStrLn $ "The words in the dictionary with the same numerical value are: " <> show ws
+
+printWords :: Natural -> S.Set T.Text -> IO ()
+printWords 0 ws = putStrLn $ "The words in the dictionary with the same numerical value are: " <> (show . S.toList) ws
+printWords m ws = do
+  -- g <- getStdGen
+  -- let num = unfoldr . Just $ uniformR (0, S.size ws - 1) g
+  undefined
+
+randomWord :: (RandomGen g) => S.Set T.Text -> g -> (T.Text, g)
+randomWord s g = (S.elemAt n s, g')
+  where
+    (n, g') = randomR (0, S.size s - 1) g
