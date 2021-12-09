e8915beb9e2f60ca61c90dd15582bd86f3e69c7f
David Sánchez <david.sanchez.lt@gmail.com>
2021-12-09 22:02:19 +0000

    Add test dict to test, prepare cleanup functions

--- hematria/test/HematriaTest.hs
+++ hematria/test/HematriaTest.hs
@@ -3,18 +3,25 @@
 module Main (main) where
 
 import qualified Data.Set as S
+import Data.Text (Text)
 import Data.Text.Hematria
 import Data.Text.Hematria.Cipher
 import Data.Text.Hematria.Dictionary
 import Test.Hspec
 
+sampleWordList :: [Text]
+sampleWordList = ["Brujería", "Viaje Astral", "Hechizos", "Conjuros", "Lujuria", "Ritual", "éter", "Ónice"]
+
+buildSampleDictionary :: IO [Text]
+buildSampleDictionary = pure sampleWordList
+
 main :: IO ()
-main = hspec spec
+main = hspec $ beforeAll cleanup spec
 
 spec :: Spec
-spec 
-  = simpleSpanishTest
-  >> simpleEnglishTest
+spec =
+  simpleSpanishTest
+    >> simpleEnglishTest
 
 simpleSpanishTest :: Spec
 simpleSpanishTest = describe "Gematria tests: Simple Spanish Cipher" $ do
@@ -25,3 +32,12 @@ simpleEnglishTest :: Spec
 simpleEnglishTest = describe "Gematria tests: Simple English Cipher" $ do
   it "Words should get the correct value for cipher" $ do
     wordWithValue EnglishSimple "Ritual" `shouldBe` (81, S.fromList ["Ritual"])
+
+cleanupCache :: IO ()
+cleanupCache = undefined -- Cleanup Cache
+
+cleanupConfig :: IO ()
+cleanupConfig = undefined -- Cleanup Config
+
+cleanup :: IO ()
+cleanup = cleanupCache <> cleanupConfig -- How does this work?
