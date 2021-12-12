ce20204467462cb0b229c37bd1b7ec1adfe896cb
David Sánchez <david.sanchez.lt@gmail.com>
2021-12-12 01:31:30 +0000

    Add Paths_ library to get data-files. Formatting.

--- hematria-gui/src/Main.hs
+++ hematria-gui/src/Main.hs
@@ -5,15 +5,16 @@ module Main where
 
 import Control.Lens
 import Data.Maybe
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import Monomer
+import qualified Monomer.Lens as L
+import Paths_hematria_gui
 import TextShow
 
-import qualified Monomer.Lens as L
-
-newtype AppModel = AppModel {
-  _clickCount :: Int
-} deriving (Eq, Show)
+newtype AppModel = AppModel
+  { _clickCount :: Int
+  }
+  deriving (Eq, Show)
 
 data AppEvent
   = AppInit
@@ -22,39 +23,42 @@ data AppEvent
 
 makeLenses 'AppModel
 
-buildUI
-  :: WidgetEnv AppModel AppEvent
-  -> AppModel
-  -> WidgetNode AppModel AppEvent
-buildUI wenv model = widgetTree where
-  widgetTree = vstack [
-      label "Hello world",
-      spacer,
-      hstack [
-        label $ "Click count: " <> showt (model ^. clickCount),
-        spacer,
-        button "Increase count" AppIncrease
-      ]
-    ] `styleBasic` [padding 10]
+buildUI ::
+  WidgetEnv AppModel AppEvent ->
+  AppModel ->
+  WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree
+  where
+    widgetTree =
+      vstack
+        [ label "Hello world",
+          spacer,
+          hstack
+            [ label $ "Click count: " <> showt (model ^. clickCount),
+              spacer,
+              button "Increase count" AppIncrease
+            ]
+        ]
+        `styleBasic` [padding 10]
 
-handleEvent
-  :: WidgetEnv AppModel AppEvent
-  -> WidgetNode AppModel AppEvent
-  -> AppModel
-  -> AppEvent
-  -> [AppEventResponse AppModel AppEvent]
+handleEvent ::
+  WidgetEnv AppModel AppEvent ->
+  WidgetNode AppModel AppEvent ->
+  AppModel ->
+  AppEvent ->
+  [AppEventResponse AppModel AppEvent]
 handleEvent wenv node model evt = case evt of
   AppInit -> []
   AppIncrease -> [Model (model & clickCount +~ 1)]
 
 main :: IO ()
 main = do
-  startApp model handleEvent buildUI config
-  where
-    config = [
-      appWindowTitle "Hello world",
-      appTheme darkTheme,
-      appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
-      appInitEvent AppInit
-      ]
-    model = AppModel 0
+  font <- getDataFileName "assets/fonts/Roboto-Regular.ttf"
+  let config =
+        [ appWindowTitle "Hello world",
+          appTheme darkTheme,
+          appFontDef "Regular" (pack font),
+          appInitEvent AppInit
+        ]
+      model = AppModel 0
+   in startApp model handleEvent buildUI config
