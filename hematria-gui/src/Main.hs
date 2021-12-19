03cac954a0a389fc8ae766396611ca88d68f1887
David Sánchez <david.sanchez.lt@gmail.com>
2021-12-19 15:43:21 +0000

    Exploring monomer tutorials

--- hematria-gui/src/Main.hs
+++ hematria-gui/src/Main.hs
@@ -13,49 +13,68 @@ import TextShow
 
 import Types
 import Data.Default (Default(def))
+import qualified Data.Text.Hematria as H
+import qualified Cache as HCache
+
+import qualified Tutorial01_Basics
+import qualified Tutorial02_Styling
+import qualified Tutorial03_LifeCycle
+import qualified Tutorial04_Tasks
+import qualified Tutorial05_Producers
+import qualified Tutorial06_Composite
+import qualified Tutorial07_CustomWidget
+import qualified Tutorial08_Themes
 
 -- newtype AppModel = AppModel
 --   { _clickCount :: Int
 --   }
 --   deriving (Eq, Show)
 
-data AppEvent
+data HematriaAppEvent
   = HematriaInit
+  | HematriaGetCache
   | HematriaPerform
   deriving (Eq, Show)
 
 -- makeLenses 'AppModel
 
 buildUI ::
-  WidgetEnv HematriaModel AppEvent ->
+  WidgetEnv HematriaModel HematriaAppEvent ->
   HematriaModel ->
-  WidgetNode HematriaModel AppEvent
+  WidgetNode HematriaModel HematriaAppEvent
 buildUI wenv model = widgetTree
   where
     widgetTree =
       vstack
         [ label "Perform Gematria:",
           spacer,
+          -- hstack
+          --   [
+          --   label "Word or phrase",
+          --   spacer,
+          --   textField
+          --   ],
           hstack
             [ label $ "Click count: ", -- <> showt (model ^. clickCount),
               spacer,
-              button "Get Dict. Cache" undefined -- AppIncrease
+              button "Get Dict. Cache" HematriaGetCache -- AppIncrease
             ]
         ]
         `styleBasic` [padding 10]
 
 handleEvent ::
-  WidgetEnv HematriaModel AppEvent ->
-  WidgetNode HematriaModel AppEvent ->
+  WidgetEnv HematriaModel HematriaAppEvent ->
+  WidgetNode HematriaModel HematriaAppEvent ->
   HematriaModel ->
-  AppEvent ->
-  [AppEventResponse HematriaModel AppEvent]
+  HematriaAppEvent ->
+  [AppEventResponse HematriaModel HematriaAppEvent]
 handleEvent wenv node model evt = case evt of
-  HematriaInit -> []
+  HematriaInit -> undefined -- [HCache.updateCache]
+  HematriaGetCache -> undefined
   HematriaPerform -> undefined -- [Model (model & clickCount +~ 1)]
 
-main :: IO ()
-main = do
+main' :: IO ()
+main' = do
   font <- getDataFileName "assets/fonts/Roboto-Regular.ttf"
   let config =
         [ appWindowTitle "Hematria",
@@ -65,3 +84,24 @@ main = do
         ]
       model = HematriaModel def
    in startApp model handleEvent buildUI config
+
+-- |
+-- Module      : Main
+-- Copyright   : (c) 2018 Francisco Vallarino
+-- License     : BSD-3-Clause (see the LICENSE file)
+-- Maintainer  : fjvallarino@gmail.com
+-- Stability   : experimental
+-- Portability : non-portable
+--
+-- Main module for the tutorials.
+
+main :: IO ()
+main = do
+  --  Tutorial01_Basics.main01
+  Tutorial02_Styling.main02
+  --  Tutorial03_LifeCycle.main03
+  --  Tutorial04_Tasks.main04
+  --  Tutorial05_Producers.main05
+  --  Tutorial06_Composite.main06
+  --  Tutorial07_CustomWidget.main07
+  --  Tutorial08_Themes.main08
---
+++ hematria-gui/src/Tutorial01_Basics.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+-- |
+-- Module      : Tutorial01_Basics
+-- Copyright   : (c) 2018 Francisco Vallarino
+-- License     : BSD-3-Clause (see the LICENSE file)
+-- Maintainer  : fjvallarino@gmail.com
+-- Stability   : experimental
+-- Portability : non-portable
+--
+-- Main module for the '01 - Basics' tutorial.
+module Tutorial01_Basics where
+
+import Control.Lens
+import Data.Text (Text)
+import Monomer
+import qualified Monomer.Lens as L
+import TextShow
+
+newtype AppModel = AppModel
+  { _clickCount :: Int
+  }
+  deriving (Eq, Show)
+
+data AppEvent
+  = AppInit
+  | AppIncrease
+  deriving (Eq, Show)
+
+makeLenses 'AppModel
+
+buildUI ::
+  WidgetEnv AppModel AppEvent ->
+  AppModel ->
+  WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree
+  where
+    widgetTree =
+      vstack
+        [ label "Hello world",
+          spacer,
+          hstack
+            [ label $ "Click count: " <> showt (model ^. clickCount),
+              spacer,
+              button "Increase count" AppIncrease
+            ]
+        ]
+        `styleBasic` [padding 10]
+
+handleEvent ::
+  WidgetEnv AppModel AppEvent ->
+  WidgetNode AppModel AppEvent ->
+  AppModel ->
+  AppEvent ->
+  [AppEventResponse AppModel AppEvent]
+handleEvent wenv node model evt = case evt of
+  AppInit -> []
+  AppIncrease -> [Model (model & clickCount +~ 1)]
+
+main01 :: IO ()
+main01 = do
+  startApp model handleEvent buildUI config
+  where
+    config =
+      [ appWindowTitle "Tutorial 01 - Basics",
+        appTheme darkTheme,
+        appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
+        appInitEvent AppInit
+      ]
+    model = AppModel 0
